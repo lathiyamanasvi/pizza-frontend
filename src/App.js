029a942c5ab0a43b073f2f6fee923b94c74c2974
Ruta Mavani <you@example.com>
2024-04-03 16:38:47 +0530

    header-footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Layout from './Component/Layout';
 import Header from './Component/Header';
+import Footer from './Component/Footer';
+import Home from './Component/Home';
 import Login from './Component/Login';
+import Register from './Component/Register';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          {/* <Route path='/' element={<Header/>}></Route> */}
-          <Route path='/' element={<Header/>}></Route>
+
+          <Route path='/' element={<Layout />}>
+            <Route path='/' element={<Login />}></Route>
+            <Route path='/register' element={<Register />}></Route>
+            <Route path='/header' element={<Header />}></Route>
+            <Route path='/Footer' element={<Footer />}></Route>
+            <Route path='/home' element={<Home />}></Route>
+          </Route>
+
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/Component/Login.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom'
+
+const Login = () => {
+    useEffect(() => {
+
+    }, [])
+    return (
+        <>
+   <style dangerouslySetInnerHTML={{__html: "\n        body {\n\tdisplay: flex;\n\talign-items: center;\n\tjustify-content: center;\n\tmin-height: 100vh;\n\tbackground: #000;\n\tfont-family: system-ui;\n}\n\nimg {\n\tdisplay: block;\n\tmax-width: 100%;\n}\n\n.card {\n\twidth: 375px;\n\tmin-height: 370px;\n\tbackground: #f3f3f3;\n\tposition: relative;\n\tdisplay: flex;\n\tflex-direction: column;\n\talign-items: center;\n\tjustify-content: flex-end;\n\tborder-radius: 32px;\n\tpadding-inline: 10px;\n}\n\n.card-img {\n\talign-self: center;\n\tmax-width: 200px;\n\theight: 200px;\n\tposition: absolute;\n\ttop: -80px;\n}\n\n.nome-do-prato,\n.preco-do-prato {\n\tfont-size: 32px;\n\tcolor: #292929;\n\ttext-align: center;\n}\n\n.descricao-prato {\n\tfont-size: 18px;\n\tcolor: #616161;\n\ttext-align: center;\n}\n\n.submit{\n  padding: 7px 138px;\n  font-size: 16px;\n}\n.input-group input{\n  text-align: left;\n}\n " }} />
+
+            <div className="card p-4">
+                <div className="card-img">
+                    <img src="https://i0.wp.com/www.multarte.com.br/wp-content/uploads/2019/03/pizza-png-transparente.png?resize=600%2C600&ssl=1" alt="Imagem do prato" />
+                </div>
+                <div className="input-group mb-3 ">
+                    <span className="input-group-text me-2" id="basic-addon1">@</span>
+                    <input type="text" className="form-control" placeholder="Enter your email" aria-label="Username" aria-describedby="basic-addon1" />
+                </div>
+                <div className="input-group mb-4 ">
+                    <span className="input-group-text me-2" id="basic-addon1">@</span>
+                    <input type="text" className="form-control" placeholder="Enter your password " aria-label="Username" aria-describedby="basic-addon1" />
+                </div>
+
+                <div>
+                    <button type="button" class="btn btn-warning submit mb-3">Submit</button>
+                </div>
+                <Link to="/register">Registered</Link>
+            </div>
+        </>
+    )
+}
+
+export default Login;
